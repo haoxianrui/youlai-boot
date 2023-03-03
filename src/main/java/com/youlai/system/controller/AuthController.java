fd12734d1bafd2128b13a30f0606a320b0583c0b
haoxr <1490493387@qq.com>
2023-03-03 18:19:17 +0800

    refactor: 接口文档添加example值

--- src/main/java/com/youlai/system/controller/AuthController.java
+++ src/main/java/com/youlai/system/controller/AuthController.java
@@ -25,8 +25,8 @@ public class AuthController {
     @Operation(summary = "登录")
     @PostMapping("/login")
     public Result<LoginResult> login(
-            @Parameter( description= "用户名",example = "admin") @RequestParam String username,
-            @Parameter(description = "密码") @RequestParam String password
+            @Parameter(description = "用户名", example = "admin") @RequestParam String username,
+            @Parameter(description = "密码",example = "123456") @RequestParam String password
     ) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 username.toLowerCase().trim(),
