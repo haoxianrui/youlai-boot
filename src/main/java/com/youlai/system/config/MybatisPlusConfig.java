6f720c546dd447f29476ebd918fe59b0cb40838b
haoxr <1490493387@qq.com>
2023-05-18 08:06:39 +0800

    docs: 注释完善

--- src/main/java/com/youlai/system/config/MybatisPlusConfig.java
+++ src/main/java/com/youlai/system/config/MybatisPlusConfig.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.extension.plugins.inner.DataPermissionIntercepto
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.youlai.system.framework.mybatisplus.MyDataPermissionHandler;
 import com.youlai.system.framework.mybatisplus.MyMetaObjectHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,11 +16,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * mybatis-plus 配置类
  *
  * @author haoxr
- * @date 2022/7/2
+ * @since 2022/7/2
  */
 @Configuration
 @EnableTransactionManagement
 public class MybatisPlusConfig {
+
+
+    @Value("${system-config.data-permission.enabled}")
+    private Boolean dataPermissionEnabled;
 
     /**
      * 分页插件和数据权限插件
@@ -28,7 +33,9 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         //数据权限
-        interceptor.addInnerInterceptor(new DataPermissionInterceptor(new MyDataPermissionHandler()));
+        if (dataPermissionEnabled) {
+            interceptor.addInnerInterceptor(new DataPermissionInterceptor(new MyDataPermissionHandler()));
+        }
         //分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
 
