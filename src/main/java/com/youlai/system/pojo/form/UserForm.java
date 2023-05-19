d3e75951f1a052b8a832d1753b0a6302eab802e7
haoxr <1490493387@qq.com>
2023-05-19 23:57:32 +0800

    refactor: 空值不进行正则校验

--- src/main/java/com/youlai/system/pojo/form/UserForm.java
+++ src/main/java/com/youlai/system/pojo/form/UserForm.java
@@ -29,7 +29,9 @@ public class UserForm {
     @NotBlank(message = "昵称不能为空")
     private String nickname;
 
-    @Pattern(regexp = "^1(3\\d|4[5-9]|5[0-35-9]|6[2567]|7[0-8]|8\\d|9[0-35-9])\\d{8}$", message = "手机号码格式不正确")
+
+    @Schema(description="手机号码")
+    @Pattern(regexp = "^$|^1(3\\d|4[5-9]|5[0-35-9]|6[2567]|7[0-8]|8\\d|9[0-35-9])\\d{8}$", message = "手机号码格式不正确")
     private String mobile;
 
     @Schema(description="性别")
