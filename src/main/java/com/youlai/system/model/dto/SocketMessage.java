77960dcf818bd93d02897e82bf71b68cdec5a73b
haoxr <1490493387@qq.com>
2023-10-17 21:48:12 +0800

    docs(SocketMessage.java): 完善注释

--- src/main/java/com/youlai/system/model/dto/SocketMessage.java
+++ src/main/java/com/youlai/system/model/dto/SocketMessage.java
@@ -1,16 +1,25 @@
-package com.youlai.system.websocket.dto;
+package com.youlai.system.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * WebSocket 消息体
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class MessageDTO {
+public class SocketMessage {
 
+    /**
+     * 发送者
+     */
     private String sender;
 
+    /**
+     * 消息内容
+     */
     private String content;
 
 }
