6b65644d9e3c058bbe88926b3d064147a388d41a
haoxr <1490493387@qq.com>
2024-11-19 00:01:28 +0800

    feat: 添加字段校验器

---
+++ src/main/java/com/youlai/boot/core/annotation/ValidField.java
@@ -0,0 +1,28 @@
+package com.youlai.boot.core.annotation;
+
+
+import com.youlai.boot.core.validator.FieldValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = FieldValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidField {
+
+
+    String message() default "非法字段";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * 允许的字段值
+     */
+    String[] allowedValues();
+
+}
