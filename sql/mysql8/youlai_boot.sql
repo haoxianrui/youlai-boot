f70c778318e3cfdaa5937741a7252ee85bac94ad
haoxr <1490493387@qq.com>
2024-12-10 12:35:58 +0800

    fix: 修复数据库缺失默认排序规则为 utf8mb4_general_ci

--- sql/mysql8/youlai_boot.sql
+++ sql/mysql8/youlai_boot.sql
@@ -7,7 +7,7 @@
     -- ----------------------------
     -- 1. 创建数据库
     -- ----------------------------
-    CREATE DATABASE IF NOT EXISTS youlai_boot DEFAULT CHARACTER SET utf8mb4 DEFAULT ;
+    CREATE DATABASE IF NOT EXISTS youlai_boot DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
 
 
     -- ----------------------------
