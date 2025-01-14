e43883f7c2a60010c087577cffa296baa9e29fb4
Ray.Hao <1490493387@qq.com>
2025-01-14 20:17:18 +0800

    docs: 完善注释

--- src/main/java/com/youlai/boot/system/mapper/UserMapper.java
+++ src/main/java/com/youlai/boot/system/mapper/UserMapper.java
@@ -14,9 +14,9 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 /**
- * 用户持久层
+ * 用户持久层接口
  *
- * @author haoxr
+ * @author Ray.Hao
  * @since 2022/1/14
  */
 @Mapper
@@ -25,9 +25,9 @@ public interface UserMapper extends BaseMapper<User> {
     /**
      * 获取用户分页列表
      *
-     * @param page
+     * @param page       分页参数
      * @param queryParams 查询参数
-     * @return
+     * @return 用户分页列表
      */
     @DataPermission(deptAlias = "u")
     Page<UserBO> getUserPage(Page<UserBO> page, UserPageQuery queryParams);
@@ -36,15 +36,15 @@ public interface UserMapper extends BaseMapper<User> {
      * 获取用户表单详情
      *
      * @param userId 用户ID
-     * @return
+     * @return 用户表单详情
      */
     UserForm getUserFormData(Long userId);
 
     /**
      * 根据用户名获取认证信息
      *
-     * @param username
-     * @return
+     * @param username 用户名
+     * @return 认证信息
      */
     UserAuthInfo getUserAuthInfo(String username);
 
@@ -52,23 +52,23 @@ public interface UserMapper extends BaseMapper<User> {
      * 根据微信openid获取用户认证信息
      *
      * @param openid 微信openid
-     * @return
+     * @return 认证信息
      */
     UserAuthInfo getUserAuthInfoByOpenId(String openid);
 
     /**
      * 根据手机号获取用户认证信息
      *
-     * @param mobile
-     * @return
+     * @param mobile 手机号
+     * @return 认证信息
      */
     UserAuthInfo getUserAuthInfoByMobile(String mobile);
 
     /**
      * 获取导出用户列表
      *
-     * @param queryParams
-     * @return
+     * @param queryParams 查询参数
+     * @return 导出用户列表
      */
     @DataPermission(deptAlias = "u")
     List<UserExportDTO> listExportUsers(UserPageQuery queryParams);
@@ -77,7 +77,7 @@ public interface UserMapper extends BaseMapper<User> {
      * 获取用户个人中心信息
      *
      * @param userId 用户ID
-     * @return
+     * @return 用户个人中心信息
      */
     UserBO getUserProfile(Long userId);
 
