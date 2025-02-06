b0ede31721b3687518ed51b769cdff6a29d8e9a5
Ray.Hao <1490493387@qq.com>
2025-02-06 13:44:12 +0800

    chore: 移除无用字段

--- src/main/java/com/youlai/boot/system/model/vo/VisitTrendVO.java
+++ src/main/java/com/youlai/boot/system/model/vo/VisitTrendVO.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * 访问趋势VO
  *
- * @author Ray
+ * @author Ray.Hao
  * @since 2.3.0
  */
 @Schema(description = "访问趋势VO")
@@ -23,9 +23,6 @@ public class VisitTrendVO {
 
     @Schema(description = "浏览量(PV)")
     private List<Integer> pvList;
-
-    @Schema(description = "访客数(UV)")
-    private List<Integer> uvList;
 
     @Schema(description = "IP数")
     private List<Integer> ipList;
